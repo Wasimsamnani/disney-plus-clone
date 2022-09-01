7398ad6b9ee02f9e7b7925490c6190e281bc9f62
wasim samnani <wasimsamnani110@gmail.com>
2022-09-01 16:46:39 +0530

    ADD viewers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import ImageSlider from './components/ImageSlider';
 
 function App() {
   return (
-    <div className="App"> 
+    <div className="App">
+      <Header/>
+      <Home/>
     </div>
   );
 }
